f75e66a202d13254cb4eb5958ea78056d5477976
jjm2473 <1129525450@qq.com>
2022-01-19 11:33:04 +0800

    support i18n alias

---
+++ luci-alias.mk
@@ -0,0 +1,8 @@
+
+LUCI_LANG.zh-cn=简体中文 (Chinese Simplified)
+LUCI_LANG.zh-tw=繁體中文 (Chinese Traditional)
+LUCI_LANG.pt-br=Português do Brasil (Brazilian Portuguese)
+LUCI_LANG.no=Norsk (Norwegian)
+LUCI_LANG.bn=বাংলা (Bengali)
+
+include $(TOPDIR)/feeds/luci/luci.mk
