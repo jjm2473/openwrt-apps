419e5d430889e735c0175c38b37b43a7b5be3399
jjm2473 <1129525450@qq.com>
2022-06-08 11:58:40 +0800

    luci-app-cpufreq: wan port hw acct switch

--- luci-app-cpufreq/luasrc/model/cbi/cpufreq/net.lua
+++ luci-app-cpufreq/luasrc/model/cbi/cpufreq/net.lua
@@ -11,7 +11,11 @@ s = m:section(TypedSection, "hw_acct", translate("Hardware Acceleration"))
 s.addremove=false
 s.anonymous=true
 
+o = s:option(Flag, "wan_acct", translate("Enable WAN Port Acceleration"), translate("Improve built-in WAN port (eth0) TX performace"))
+o.default="1"
+
 o = s:option(Flag, "hw_pppoe", translate("Enable PPPoE Acceleration"), translate("Improve PPPoE TX performace, only support built-in NICs"))
 o.rmempty=true
+o:depends('wan_acct', 1)
 
 return m
