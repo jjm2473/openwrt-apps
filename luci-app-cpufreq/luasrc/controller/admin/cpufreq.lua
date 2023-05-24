256488722edbee67d6d7b086521a8e28633cb342
jjm2473 <1129525450@qq.com>
2023-05-24 13:31:17 +0800

    luci-app-cpufreq: remove fan control function

--- luci-app-cpufreq/luasrc/controller/admin/cpufreq.lua
+++ luci-app-cpufreq/luasrc/controller/admin/cpufreq.lua
@@ -48,13 +48,10 @@ end
 
 function get_cpu_info()
   local fs = require "nixio.fs"
-  -- fs.readfile("/sys/devices/system/cpu/cpufreq/policy0/scaling_governor")
-  -- local util = require "luci.util"; util.split(fs.readfile("/sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors"), " ")
   local freq = tonumber(fs.readfile("/sys/devices/system/cpu/cpufreq/policy0/cpuinfo_cur_freq")) / 1000; -- MHz
-  local temp = tonumber(fs.readfile("/sys/class/thermal/thermal_zone0/temp")) / 1000; -- ˚C
   luci.http.status(200, "ok")
   luci.http.prepare_content("application/json")
-  luci.http.write_json({freq=freq, temp=temp})
+  luci.http.write_json({freq=freq})
 end
 
 function sandbox_index(param)
--- luci-app-cpufreq/luasrc/model/cbi/cpufreq/main.lua
+++ luci-app-cpufreq/luasrc/model/cbi/cpufreq/main.lua
@@ -7,13 +7,10 @@ local m, s, o
 require "luci.util"
 
 local governor_path = "/sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
-local type0_path = "/sys/devices/virtual/thermal/thermal_zone0/trip_point_0_type"
-local temp0_path = "/sys/devices/virtual/thermal/thermal_zone0/trip_point_0_temp"
-local hyst0_path = "/sys/devices/virtual/thermal/thermal_zone0/trip_point_0_hyst"
 
 local fs = require "nixio.fs"
 
-m = Map("cpufreq", nil, translate("Manage CPU performance and temperature over LuCI."))
+m = Map("cpufreq", nil, translate("Manage CPU performance over LuCI."))
 m:section(SimpleSection).template  = "cpufreq/cpuinfo"
 
 s = m:section(TypedSection, "cpufreq")
@@ -28,23 +25,5 @@ for i,v in pairs(luci.util.split(luci.util.trim(fs.readfile("/sys/devices/system
 end
 o.rmempty = false
 o.default = cur_governor
-
-if fs.access(type0_path) and "active" == luci.util.trim(fs.readfile(type0_path)) then
-  if math.floor(fs.stat(temp0_path, "modedec")/200) % 2 == 1 then
-    local cur_trip_temp = luci.util.trim(fs.readfile(temp0_path))
-    o = s:option(Value, "fan_temp", translate("Fan trigger temperature") .. " (&#8451;)")
-    o.datatype = "uinteger"
-    o.rmempty = false
-    o.default = tonumber(cur_trip_temp)/1000
-  end
-
-  if math.floor(fs.stat(hyst0_path, "modedec")/200) % 2 == 1 then
-    local cur_trip_hyst = luci.util.trim(fs.readfile(hyst0_path))
-    o = s:option(Value, "fan_hyst", translate("Fan temperature hysteresis") .. " (&#8451;)", translate("Set a larger value to avoid frequent restart of the fan"))
-    o.datatype = "and(uinteger,min(3))"
-    o.rmempty = false
-    o.default = tonumber(cur_trip_hyst)/1000
-  end
-end
 
 return m
