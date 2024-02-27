dad60db25e0fda59f0572a7f97d6509e31ac8a13
jjm2473 <1129525450@qq.com>
2024-02-27 14:56:40 +0800

    luci-app-cpufreq: ipk mirror add more mirrors

--- luci-app-cpufreq/luasrc/model/cbi/cpufreq/ipk.lua
+++ luci-app-cpufreq/luasrc/model/cbi/cpufreq/ipk.lua
@@ -13,12 +13,20 @@ s.anonymous=true
 
 o = s:option(ListValue, "mirror", translate("Mirror server"))
 o:value("disable", "")
-o:value("http://downloads.openwrt.org/", translate("OpenWrt") .. " (HTTP)")
-o:value("https://downloads.openwrt.org/", translate("OpenWrt") .. " (HTTPS)")
+o:value("http://downloads.openwrt.org/", translate("OpenWRT") .. " (HTTP)")
+o:value("https://downloads.openwrt.org/", translate("OpenWRT") .. " (HTTPS)")
+o:value("https://mirrors.cernet.edu.cn/openwrt/", translate("CERNET 302"))
+o:value("https://mirrors.sustech.edu.cn/openwrt/", translate("SUSTech"))
 o:value("https://mirrors.tuna.tsinghua.edu.cn/openwrt/", translate("Tsinghua University"))
 o:value("https://mirrors.ustc.edu.cn/openwrt/", translate("USTC"))
+o:value("https://mirror.lzu.edu.cn/openwrt/", translate("Lanzhou University"))
 o:value("https://mirrors.aliyun.com/openwrt/", translate("Alibaba Cloud"))
 o:value("https://mirrors.cloud.tencent.com/openwrt/", translate("Tencent Cloud"))
+o:value("https://mirror.iscas.ac.cn/openwrt/", translate("Chinese Academy of Sciences"))
+o:value("https://mirror.nyist.edu.cn/openwrt/", translate("NYIST"))
+o:value("https://mirror.sjtu.edu.cn/openwrt/", translate("SJTU"))
+o:value("https://mirrors.cqupt.edu.cn/openwrt/", translate("CQUPT"))
+o:value("https://mirrors.qlu.edu.cn/openwrt/", translate("Qilu University of Technology"))
 
 o.rmempty = false
 o.default = "disable"
